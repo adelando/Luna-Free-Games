2d87e3674c7018acf10629c2dd2c29a301bf1313
Adelando <adelando@users.noreply.github.com>
2026-01-02 10:36:35 +0800

    fix to config_flow

--- custom_components/amazon-luna/config_flow.py
+++ custom_components/amazon-luna/config_flow.py
@@ -1,5 +1,5 @@
 from homeassistant import config_entries
-import voluptuous as os
+import voluptuous as vol
 from .const import DOMAIN
 
 class LunaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -8,10 +8,13 @@ class LunaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step when user adds via UI."""
+        # Check if already configured
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
 
         if user_input is not None:
+            # This creates the entry in the UI
             return self.async_create_entry(title="Amazon Luna Games", data={})
 
-        return self.async_show_form(step_id="user")
+        # Show the empty form (just a 'Submit' button)
+        return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
