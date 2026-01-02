3a66b853c95acf3e647ac6ebc9f22529cd63dacc
Adelando <adelando@users.noreply.github.com>
2026-01-03 05:53:24 +0800

    Update const.py

--- custom_components/amazon-luna/const.py
+++ custom_components/amazon-luna/const.py
@@ -1,3 +1,2 @@
 DOMAIN = "amazon_luna"
-SCAN_INTERVAL_HOURS = 12  # Games don't change often
-SOURCE_URL = "https://clouddosage.com/gamelists/free-games-on-amazon-luna-with-amazon-prime/"
+SOURCE_URL = "https://luna.amazon.com/claims/home"
