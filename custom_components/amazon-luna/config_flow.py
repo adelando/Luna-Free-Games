a61128c3bc0f47a8f921d4fb2b95c10ce5c8e2f5
Adelando <adelando@users.noreply.github.com>
2026-01-03 08:09:10 +0800

    Update config_flow.py

--- custom_components/amazon-luna/config_flow.py
+++ custom_components/amazon-luna/config_flow.py
@@ -7,16 +7,11 @@ class LunaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step. No input required."""
+        """Step one: Click submit to finish."""
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
 
         if user_input is not None:
-            # When the user clicks Submit, this creates the entry
             return self.async_create_entry(title="Amazon Luna Games", data={})
 
-        # Return an empty schema to show a 'Submit' only form
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({})
-        )
+        return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
